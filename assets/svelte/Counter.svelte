56402046f945b0c8b8f9a8b2b013e91925bba7c0
learnuidev <learnuidev@gmail.com>
2025-03-12 12:51:45 -0400

    added prettierrc

---
+++ assets/svelte/Counter.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let counter = 0;
+  import NumberFlow from "@number-flow/svelte";
+</script>
+
+<div>
+  <h1 class="text-center text-9xl">
+    <NumberFlow value={counter} />
+  </h1>
+
+  <div class="flex justify-center items-center gap-12 mt-12 text-3xl">
+    <button phx-click="increment">Increment</button>
+    <button phx-click="decrement">Decrement</button>
+    <button phx-click="reset">Reset</button>
+  </div>
+</div>
--- lib/svelte_demo_web/live/svelte_live.ex
+++ lib/svelte_demo_web/live/svelte_live.ex
@@ -20,17 +20,7 @@ defmodule SvelteDemoWeb.SvelteLive do
   @spec render(any()) :: Phoenix.LiveView.Rendered.t()
   def render(assigns) do
     ~H"""
-    <div>
-      <h1 class="text-center text-9xl">
-        {@counter}
-      </h1>
-
-      <div class="flex justify-center items-center gap-12 mt-12 text-3xl">
-        <button phx-click="increment">Increment</button>
-        <button phx-click="decrement">Decrement</button>
-        <button phx-click="reset">Reset</button>
-      </div>
-    </div>
+    <.svelte name="Counter" props={%{counter: @counter}} />
     """
   end
 
